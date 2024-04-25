92847e1e2fc1887aef007db0638b6eeef94e13b1
eduardomarra <eduardomarra@gmail.com>
2024-04-25 17:38:39 -0300

    feat: cerated product entity

---
+++ src/main/java/com/emarra/crudProduct/entities/Product.java
@@ -0,0 +1,66 @@
+package com.emarra.crudProduct.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Integer price_in_cents;
+    private String imgUrl;
+
+    public Product(){}
+
+    public Product(Long id, String name, String description, Integer price_in_cents, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price_in_cents = price_in_cents;
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice_in_cents() {
+        return price_in_cents;
+    }
+
+    public void setPrice_in_cents(Integer price_in_cents) {
+        this.price_in_cents = price_in_cents;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
